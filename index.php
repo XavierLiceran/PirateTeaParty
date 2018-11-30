b5b360b454f29637c18f24d8cbb5f6c87979d636
danireyes5015@gmail.com <50138013>
2018-11-30 16:10:41 +0100

    diseño casi final 30-11-2018
    dani: fondo + template + footer casi def

--- index.php
+++ index.php
@@ -1,47 +1,94 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] .'/PirateTeaParty/templates/master.php' ?>
 <script src="js/miJS/prueba.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Bootstrap Core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+<!-- Theme CSS -->
+<link href="css/agency.min.css" rel="stylesheet">
 
 <?php startblock('titulo')?>
     Index
 <?php endblock()?>
 
 <?php startblock('principal')?>
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Header -->
+    <header id="principal-header">
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Hola!</div>
+                <div class="intro-heading">Bienvenidos/as a la aventura sobre los piratas en el mediterraneo!</div>
+                <a href="#services" class="page-scroll btn btn-xl">Quiero unirme!</a>
+            </div>
+        </div>
+    </header>
 
-<!-- indicators -->
-<ol class="carousel-indicators">
-  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-</ol>
+    <!-- Services Section -->
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Comienza la aventura , registrate Arg!</h2>
+                    <h3 class="section-subheading text-muted">(Es gratis , y lo seguirá siendo)</h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Es gratis y para todos los públicos</h4>
+                    <p class="text-muted">Nuestra intención es meramente didàctica a la vez que entretenida.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Responsive Design</h4>
+                    <p class="text-muted">Funciona perfectamente en cualquier plataforma (Pc,móvil).</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Tus datos están seguros con nosotros</h4>
+                    <p class="text-muted">Tomamos las medidas para que no hayan robos de tus datos.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<!-- inner -->
-<div class="carousel-inner" role="listbox">
-  <div class="item active">
-    <img src="img/pirata1.jpg"" alt="Preparado para saber más sobre piratas?">
-         
-  </div>
-  <div class="item">
-    <img src="img/pirata2.jpg"  alt="Pues bienvenido! a PirateTeaParty!">
-  </div>
-  <div class="item">
-    <img src="img/pirata3.jpg"  alt="Registrate en el botón de abajo _:">        
-  </div>
-</div>
 
-<!-- controls -->
-<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-  <span class="sr-only">Previous</span>
-</a>
-<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-  <span class="sr-only">Next</span>
-</a>
+    
 
-</div>
-<footer>
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script>
+
+<!-- Theme JavaScript -->
+<script src="js/agency.min.js"></script>
+
+
+<footer class="mt-1">
 <div style="background-color: #7FCEC5;">
   <div class="container">
 
@@ -57,12 +104,10 @@
       <!-- Grid column -->
       <div class="col-md-6 col-lg-7 text-center text-md-right">
 
-            <a href="#" class="fa fa-facebook"></a>
-            <a href="#" class="fa fa-twitter"></a>
-            <a href="#" class="fa fa-google"></a>
-            <a href="#" class="fa fa-linkedin"></a>
-            <a href="#" class="fa fa-youtube"></a>
-            <a href="#" class="fa fa-instagram"></a>
+            <a href="https://wwww.facebook.com" target="_blank" class="fa fa-facebook"></a>
+            <a href="https://twitter.com/" target="_blank" class="fa fa-twitter"></a>
+            <a href="https://www.instagram.com" target="_blank" class="fa fa-instagram"></a>
+            <a href=" https://www.youtube.com/" target="_blank" class="fa fa-youtube"></a>
 
       </div>
 
