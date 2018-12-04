2284fe175d222d8103b658e3ecd772601f366fce
MasterLice <33721976+MasterLice@users.noreply.github.com>
2018-12-04 23:11:01 +0100

    Register Modal funciona correctamente

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 <script src="vendor/jquery/jquery.min.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- <script src="vendor/bootstrap/js/bootstrap.min.js"></script> -->
 
 <!-- Plugin JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
@@ -144,6 +144,6 @@
   <a href="https://mdbootstrap.com/education/bootstrap/"> PirateTeaParty.com </a>  Todos los derechos reservados.
 </div>
 </footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> -->
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> -->
 <?php endblock()?>
