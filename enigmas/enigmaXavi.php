9f4c318ce26a8ef21d56495a53929b4824fc8d67
MasterLice <33721976+MasterLice@users.noreply.github.com>
2018-12-05 01:08:15 +0100

    Create enigmaXavi.php

---
+++ enigmas/enigmaXavi.php
@@ -0,0 +1,9 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] .'/PirateTeaParty/templates/master.php' ?>
+
+<?php startblock('titulo')?>
+    Enigma Xavi
+<?php endblock()?>
+
+<?php startblock('principal')?>
+    
+<?php endblock()?>
